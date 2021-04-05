9392adf5bc7e534b75053b14dc64c3e215c1a185
ManuelPinto90 <79107003+ManuelPinto90@users.noreply.github.com>
2021-04-05 20:50:19 +0200

    Login y Registro

--- Anthill/src/app/app-routing.module.ts
+++ Anthill/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"about", component:AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
