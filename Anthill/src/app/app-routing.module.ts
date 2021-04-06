022f4a1dfadb47d4d5b392cf9766e21bed502f5e
Bermejo3 <alberto.bermejo.corredor@gmail.com>
2021-04-06 10:35:26 +0200

    sidebar header temporal

--- Anthill/src/app/app-routing.module.ts
+++ Anthill/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {path:"", component:LoginComponent},
   {path:"register", component:RegisterComponent},
-  {path:"about", component:AboutUsComponent}
+  {path:"about", component:AboutUsComponent},
+  {path:"home", component:HomeComponent}
 ];
 
 @NgModule({
--- Anthill/src/app/pages/header/header.component.ts
+++ Anthill/src/app/pages/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+​
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  private now = new Date()
+  public hour: number = this.now.getHours()
+  public minute: number = this.now.getMinutes()
+  public seconds: number = this.now.getSeconds()
+  public weekDay = this.now.getDay()
+  public day = this.now.getDate()
+  public month = this.now.getMonth()
   constructor() { }
-
+​
   ngOnInit(): void {
   }
+  public getDay(){
+    let weekD = ["Domingo","Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
+    let monthM = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+    return weekD[this.weekDay] +", " + this.day +" de " +monthM[this.month]
+  }
+​
 
 }
