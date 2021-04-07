f54187de772fb75f22278d447a18180b07f08681
Rosana Ocaña Perez <carlosocana91@gmail.com>
2021-04-07 21:04:27 +0200

    header, sidebar, produccion

--- Anthill/src/app/pages/produccion/produccion.component.ts
+++ Anthill/src/app/pages/produccion/produccion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxEchartsModule } from 'ngx-echarts';
+
 
 @Component({
   selector: 'app-produccion',
@@ -6,6 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produccion.component.css']
 })
 export class ProduccionComponent implements OnInit {
+
+  chartOptions: NgxEchartsModule = {
+    color: ["#ff9100"],
+    title: 
+    {
+      textStyle:{color:'#ffffff'},
+      text: 'Productividad',
+    },
+    textStyle: 
+    {
+      color: '#ffffff'
+    },
+    tooltip: 
+    {
+      trigger: 'axis',
+      axisPointer: 
+      {
+          type: 'cross',
+          label: 
+          {
+            backgroundColor: '#6a7985'
+          }
+      }
+    },
+    legend: { 
+      textStyle:{color:'#ffffff'},
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            boundaryGap: false,
+            data: ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value'
+        }
+    ],
+    series: [
+        {   
+            name: 'Productividad Total',
+            type: 'line',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [120, 132, 101, 134, 90, 230, 210]
+        }
+  ]}
 
   constructor() { }
 
