13b375b56da2dce18d146928f98ddc3a21cd5477
Bermejo3 <alberto.bermejo.corredor@gmail.com>
2021-04-12 21:03:30 +0200

    comit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + 'dist/Anthill'));
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + 'dist/Anthill'));
+    res.sendFile(path.join(__dirname, 'dist/Anthill'));
 })
 
 app.listen(process.env.PORT || 8080)
